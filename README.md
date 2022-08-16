4d596fc0df18ef7b303d3123c406664e1a6844fc
plaristote <michael@unetresgrossebite.com>
2022-08-16 23:49:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# libcrails-html-views
+
+C++ library
---
+++ libcrails-html-views/renderers/html_renderer.hpp
@@ -0,0 +1,18 @@
+#ifndef  CRAILS_HTML_RENDERER_HPP
+# define CRAILS_HTML_RENDERER_HPP
+
+# include <crails/renderer.hpp>
+
+namespace Crails
+{
+  class HtmlRenderer : public Renderer
+  {
+  public:
+    HtmlRenderer();
+
+    bool can_render(const std::string& accept_header, const std::string& view) const override;
+    void render_template(const std::string& view, RenderTarget&, SharedVars& vars) const override;
+  };
+}
+
+#endif
