a39695147e1b2042ef3eea6c173d4586b154ebb9
plaristote <michael@unetresgrossebite.com>
2022-09-18 22:18:07 +0200

    update FormBuilder to allow fields with values not bound to the model's value

--- libcrails-html-views/crails/html_form_builder.hpp
+++ libcrails-html-views/crails/html_form_builder.hpp
@@ -15,12 +15,16 @@ namespace Crails
     FormBuilder(HtmlTemplate* parent, MODEL& model) : html_template(*parent), model(model) {}
     std::string label_for(const std::string& name) const { return label_for(name, [&name]() { return name; }); }
     std::string label_for(const std::string& name, HtmlTemplate::Yieldable yieldable) const { return html_template.tag("label", {{"for",scoped_name(name)}}, yieldable); }
+    std::string text_field(const std::string& name, const std::string& value, std::map<std::string,std::string> attrs = {}) const { return html_template.text_field(scoped_name(name), value, attrs); }
+    std::string text_area(const std::string& name, const std::string& value, std::map<std::string,std::string> attrs = {}) const { return html_template.text_area(scoped_name(name), value, attrs); }
+    std::string date_field(const std::string& name, std::time_t value, std::map<std::string,std::string> attrs = {}) const { return html_template.date_field(scoped_name(name), value, attrs); }
+    std::string password_field(const std::string& name, const std::string& value, std::map<std::string,std::string> attrs = {}) const { return html_template.password_field(scoped_name(name), value, attrs); }
     template<typename METHOD> std::string text_field(const std::string& name, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.text_field(scoped_name(name), (model.*method)(), attrs); }
     template<typename METHOD> std::string text_area(const std::string& name, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.text_area(scoped_name(name), (model.*method)(), attrs); }
     template<typename METHOD> std::string date_field(const std::string& name, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.date_field(scoped_name(name), (model.*method)(), attrs); }
     template<typename METHOD> std::string number_field(const std::string& name, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.number_field(scoped_name(name), (model.*method)(), attrs); }
     template<typename METHOD> std::string password_field(const std::string& name, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.password_field(scoped_name(name), (model.*method)(), attrs); }
-    template<typename VALUE, typename METHOD> std::string select_field(const std::string& name, const std::map<VALUE, std::string>& options, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.select_field(scoped_name(name), options, (model.*method)(), attrs); }
+    template<typename VALUE, typename METHOD> std::string select_field(const std::string& name, const std::map<VALUE, std::string>& options, METHOD method, std::map<std::string,std::string> attrs = {}) const { return html_template.select_field<VALUE>(scoped_name(name), options, (model.*method)(), attrs); }
   };
 }
 
